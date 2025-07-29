60a4f920f05eb7b9177f176b10296c48d0605720
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-07-29 15:42:37 -0400

    Initial Commit

---
+++ CharacterBody2d.cs
@@ -0,0 +1,105 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class CharacterBody2d : CharacterBody2D
+{
+	public Dictionary<footware, int> footwarespeeds = new Dictionary<footware, int>() {
+		{ footware.none, 400 },
+		{ footware.test, 800 },
+	};
+	
+	public Panel panel;
+	public TextureButton test;
+	
+	public enum footware{none, test};
+	
+	public footware current;
+	
+	public int speed = 400;
+	public int defaultspeed = 400;
+	public int gravityspeed = 1300;
+	public int jumpspeed = -500;
+	public int downspeed = 800;
+	public int state = 1;
+	
+	public override void _Ready() {
+		panel = GetParent().GetNode<Panel>("CanvasLayer/Panel");
+		test = GetParent().GetNode<TextureButton>("CanvasLayer/Panel/TestFootWare");
+		current = footware.none;
+	}
+	
+	public override void _PhysicsProcess(double delta) {
+
+		Vector2 velocity = Velocity;
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if(IsOnFloor()){
+			if(Input.IsActionPressed("shift")){
+				velocity.X = direction.X * speed/3;
+			}
+			else {
+				velocity.X = direction.X * speed;
+			}
+		}
+		else{
+			velocity.X += direction.X * (speed/30);
+			if(velocity.X >= 400 || velocity.X <= -400){
+				if(velocity.X > 0){
+					velocity.X = 400;
+				}
+				else{
+					velocity.X = -400;
+				}
+			}
+		}
+		if(!IsOnFloor()){
+			velocity.Y += gravityspeed * (float)delta;
+		}
+		else{
+			velocity.Y = 0;
+		}
+		
+		if(IsOnFloor() && Input.IsActionPressed("ui_jump")){
+			velocity.Y = jumpspeed;
+		}
+		
+		if(!IsOnFloor() && Input.IsActionJustPressed("ui_up")){
+			velocity.Y += jumpspeed + 100;
+		}
+		
+		if(!IsOnFloor() && Input.IsActionPressed("ui_down")){
+			velocity.Y += downspeed * (float)delta;
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
+
+		
+		if(Input.IsActionJustPressed("e")){
+			togglemenu();
+		}
+	}
+	
+	public void togglemenu(){
+		if(panel.Visible == true){
+			Engine.TimeScale = 1.0f;
+			state = 1;
+		}
+		else {
+			Engine.TimeScale = 0.2f;
+			state = 2;
+		}
+		panel.Visible = !panel.Visible;
+	}
+	
+	public void updatefootware(){
+		speed = footwarespeeds[current];
+	}
+	
+	public void _on_test_foot_ware_pressed(){
+		current = footware.test;
+		GD.Print("yo");
+		updatefootware();
+		
+	}
+}
